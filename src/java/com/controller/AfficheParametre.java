1239bed8fcfaba168b5748e370738d48e95825ca
usager <usager@B125E14.int.crosemont.qc.ca>
2019-03-20 16:54:26 -0400

    Installation

---
+++ src/java/com/controller/AfficheParametre.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Salahudine
+ */
+public class AfficheParametre extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AfficherParameter</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1 align =center>Information du détenteur de la carte: "  + "</h1>");
+            out.println("<table border=1 align=center>\n" +
+		                "<tr bgcolor=\"#FFAD00\">\n" +
+		                "<th>Nom des parametres<th>Valeur(s) des parametres" + "</th>");
+            
+            	   Enumeration<String>  paramNames  =  request.getParameterNames();
+		   while (paramNames.hasMoreElements()) {
+			 String  paramName = (String) paramNames.nextElement();
+			 out.println("<tr><td>"  + paramName + "\n<td>");
+			 
+			 String []  paramValues =  request.getParameterValues(paramName);
+			  for (int i = 0; i < paramValues.length; i++) {
+				//String string = paramValues[i];
+				out.println(paramValues[i]);
+				
+			}
+			
+		}
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
